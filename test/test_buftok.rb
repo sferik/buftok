de772907e14cce0a65bcce86b0f78dc73b024fd3
Martin Emde <me@martinemde.com>
2013-11-20 19:01:21 -0800

    Refactor to support long delimiters that are split between chunks.
    Uses two different classes now to maintain performance for single
    character delimiters, which don't have to worry about splitting
    between chunks.

--- test/test_buftok.rb
+++ test/test_buftok.rb
@@ -4,24 +4,24 @@ require 'buftok'
 class TestBuftok < Test::Unit::TestCase
   def test_buftok
     tokenizer = BufferedTokenizer.new
-    assert_equal(tokenizer.extract("foo\nbar"), %w[foo])
-    assert_equal(tokenizer.extract("baz\nqux\nquu"), %w[barbaz qux])
-    assert_equal(tokenizer.flush, 'quu')
-    assert_equal(tokenizer.flush, '')
+    assert_equal %w[foo], tokenizer.extract("foo\nbar")
+    assert_equal %w[barbaz qux], tokenizer.extract("baz\nqux\nquu")
+    assert_equal 'quu', tokenizer.flush
+    assert_equal '', tokenizer.flush
   end
 
   def test_delimiter
     tokenizer = BufferedTokenizer.new('<>')
-    assert_equal(tokenizer.extract("<>foo\n<>"), ['', "foo\n"])
-    assert_equal(tokenizer.extract('bar<>baz'), %w[bar])
-    assert_equal(tokenizer.flush, 'baz')
+    assert_equal ['', "foo\n"], tokenizer.extract("<>foo\n<>")
+    assert_equal %w[bar], tokenizer.extract('bar<>baz')
+    assert_equal 'baz', tokenizer.flush
   end
 
   def test_split_delimiter
     tokenizer = BufferedTokenizer.new('<>')
-    assert_equal(tokenizer.extract('foo<'), [])
-    assert_equal(tokenizer.extract('>bar<'), %w[foo])
-    assert_equal(tokenizer.extract('baz<>qux<>'), %w[bar<baz qux])
-    assert_equal(tokenizer.flush, '')
+    assert_equal [], tokenizer.extract('foo<')
+    assert_equal %w[foo], tokenizer.extract('>bar<')
+    assert_equal %w[bar<baz qux], tokenizer.extract('baz<>qux<>')
+    assert_equal '', tokenizer.flush
   end
 end
